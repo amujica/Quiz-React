7769333ed8fb440f28463903f0c4daa5b8764102
amujica <36471169+amujica@users.noreply.github.com>
2018-12-14 13:09:52 +0100

    Carpeta build, construida con yarn build

---
+++ README.md
@@ -0,0 +1,2 @@
+Para ejecutar, entrar en la carpeta build (construido con yarn), y doble click en el fichero index.html.
+El código se puede ver en la carpeta quiz-React.
---
+++ build/precache-manifest.8f9f9aec69548aaf6e927b251256ebe2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "cea90175e2df78d74da0",
+    "url": "./static/js/main.cea90175.chunk.js"
+  },
+  {
+    "revision": "04a161607811233ffc54",
+    "url": "./static/js/1.04a16160.chunk.js"
+  },
+  {
+    "revision": "cea90175e2df78d74da0",
+    "url": "./static/css/main.fb59d94f.chunk.css"
+  },
+  {
+    "revision": "a54502364bf86404f9e8a7d3d9d95d17",
+    "url": "./index.html"
+  }
+];
